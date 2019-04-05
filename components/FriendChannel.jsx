b22abf3cce59a904a581908b418e1f9af69380c5
Blake <bsward8@gmail.com>
2019-04-06 03:51:55 +1100

    fix(FriendChannel.jsx): use transitionTo to move to new channel

--- components/FriendChannel.jsx
+++ components/FriendChannel.jsx
@@ -5,9 +5,23 @@ const { getDMFromUserId } = getModule([ 'getDMFromUserId' ]);
 module.exports = ({ user, status, statuses }) => (
   <div class="channel-2QD9_O pc-channel" style={{ height: '42px',
     opacity: 1 }} onClick={(e) => {
-    e.target.classList.add('selected-1HYmZZ', 'pc-selected');
+    getModule([ 'transitionTo' ]).transitionTo(`/channels/@me/${getDMFromUserId(user.id)}`);
+
+    let { target } = e;
+    const callNewTarget = () => {
+      target = target.parentNode;
+      if (![ ...target.classList ].includes('pc-channel')) {
+        callNewTarget();
+      }
+    };
+    callNewTarget();
+
+    for (const elm of [ ...document.querySelectorAll('.pc-selected') ]) {
+      elm.classList.remove('selected-1HYmZZ', 'pc-selected');
+    }
+    target.classList.add('selected-1HYmZZ', 'pc-selected');
   }}>
-    <a href={`/channels/@me/${getDMFromUserId(user.id)}`}>
+    <a>
       <div class="wrapper-2F3Zv8 pc-wrapper small-5Os1Bb pc-small forceDarkTheme-2cI4Hb pc-forceDarkTheme avatar-28BJzY pc-avatar avatarSmall-3ACRaI">
         <div user={user.username} status={status} class="inner-1W0Bkn pc-inner stop-animation" style={{ backgroundImage: `url(${user.avatarURL})` }}></div>
         <div class={`${statuses[status].class} status-oxiHuE pc-${status} pc-status small-5Os1Bb pc-small status-2zcSVk pc-status status-1ibiUI pc-status`}></div>
