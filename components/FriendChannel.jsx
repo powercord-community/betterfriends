94b3a01b12c7ba81ad4d428be9ba53da71c8a1bd
Aaron Lee Marais <aaronleem@gmail.com>
2019-04-05 20:39:26 +0200

    Use <a> tag correctly (get the href from the tag instead of generating in code) Abstract out the onclick method

--- components/FriendChannel.jsx
+++ components/FriendChannel.jsx
@@ -1,11 +1,20 @@
 const { React } = require('powercord/webpack');
 const { getModule } = require('powercord/webpack');
 const { getDMFromUserId } = getModule([ 'getDMFromUserId' ]);
+const { transitionTo } = getModule([ 'transitionTo' ])
 
-module.exports = ({ user, status, statuses }) => (
-  <div class="channel-2QD9_O pc-channel" style={{ height: '42px',
-    opacity: 1 }} onClick={(e) => {
-    getModule([ 'transitionTo' ]).transitionTo(`/channels/@me/${getDMFromUserId(user.id)}`);
+module.exports = class BetterFriendChannel extends React.Component {
+  constructor ({ user, status, statuses }) {
+    super();
+
+    this.user = user;
+    this.status = status;
+    this.statuses = statuses;
+  }
+
+  // no usage of "this", no need to bind
+  onclick(e) {
+    e.preventDefault();
 
     let { target } = e;
     const callNewTarget = () => {
@@ -16,17 +25,24 @@ module.exports = ({ user, status, statuses }) => (
     };
     callNewTarget();
 
-    for (const elm of [ ...document.querySelectorAll('.pc-selected') ]) {
+    transitionTo(target.firstChild.getAttribute("href"));
+    for (const elm of [ ...document.querySelectorAll('.pc-selected .pc-friendchannel') ]) {
       elm.classList.remove('selected-1HYmZZ', 'pc-selected');
     }
     target.classList.add('selected-1HYmZZ', 'pc-selected');
-  }}>
-    <a>
-      <div class="wrapper-2F3Zv8 pc-wrapper small-5Os1Bb pc-small forceDarkTheme-2cI4Hb pc-forceDarkTheme avatar-28BJzY pc-avatar avatarSmall-3ACRaI">
-        <div user={user.username} status={status} class="inner-1W0Bkn pc-inner stop-animation" style={{ backgroundImage: `url(${user.avatarURL})` }}></div>
-        <div class={`${statuses[status].class} status-oxiHuE pc-${status} pc-status small-5Os1Bb pc-small status-2zcSVk pc-status status-1ibiUI pc-status`}></div>
+  }
+
+  render() {
+    return (
+      <div class="channel-2QD9_O pc-channel pc-friendchannel" style={{ height: '42px', opacity: 1 }}>
+        <a href={`/channels/@me/${getDMFromUserId(this.user.id)}`} onClick={this.onclick}>
+          <div class="wrapper-2F3Zv8 pc-wrapper small-5Os1Bb pc-small forceDarkTheme-2cI4Hb pc-forceDarkTheme avatar-28BJzY pc-avatar avatarSmall-3ACRaI">
+            <div user={this.user.username} status={this.status} class="inner-1W0Bkn pc-inner stop-animation" style={{ backgroundImage: `url(${this.user.avatarURL})` }}></div>
+            <div class={`${this.statuses[this.status].class} status-oxiHuE pc-${this.status} pc-status small-5Os1Bb pc-small status-2zcSVk pc-status status-1ibiUI pc-status`}></div>
+          </div>
+          <div class="nameWrapper-10v56U pc-nameWrapper"><span class="name-2WpE7M pc-name">{this.user.username}</span></div>
+        </a>
       </div>
-      <div class="nameWrapper-10v56U pc-nameWrapper"><span class="name-2WpE7M pc-name">{user.username}</span></div>
-    </a>
-  </div>
-);
+    );
+  }
+}
