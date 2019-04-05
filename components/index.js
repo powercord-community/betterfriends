a86df39214ae048f4eaa5fc142f2f4dc56d160ef
Blake <bsward8@gmail.com>
2019-04-06 01:33:26 +1100

    feat: index file for components

---
+++ components/index.js
@@ -0,0 +1,8 @@
+require('fs')
+  .readdirSync(__dirname)
+  .filter(file => file !== 'index.js')
+  .forEach(filename => {
+    const moduleName = filename.split('.')[0];
+    exports[moduleName] = require(`${__dirname}/${filename}`);
+  });
+
