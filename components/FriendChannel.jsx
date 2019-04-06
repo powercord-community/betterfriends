14872cef201afa17f481253b42696df6c5c36905
Blake <bsward8@gmail.com>
2019-04-07 01:38:38 +1100

    feat: general fixes + information handling

--- components/FriendChannel.jsx
+++ components/FriendChannel.jsx
@@ -1,8 +1,10 @@
 const { React } = require('powercord/webpack');
 const { getModule } = require('powercord/webpack');
 const { getDMFromUserId } = getModule([ 'getDMFromUserId' ]);
+const { open: openModal } = require('powercord/modal');
+const InformationModal = require('./InformationModal');
 
-module.exports = ({ user, status, statuses }) => (
+module.exports = ({ user, status, statuses, data }) => (
   <div class="channel-2QD9_O pc-channel" style={{ height: '42px',
     opacity: 1 }} onClick={(e) => {
     getModule([ 'transitionTo' ]).transitionTo(`/channels/@me/${getDMFromUserId(user.id)}`);
@@ -27,6 +29,16 @@ module.exports = ({ user, status, statuses }) => (
         <div class={`${statuses[status].class} status-oxiHuE pc-${status} pc-status small-5Os1Bb pc-small status-2zcSVk pc-status status-1ibiUI pc-status`}></div>
       </div>
       <div class="nameWrapper-10v56U pc-nameWrapper"><span class="name-2WpE7M pc-name">{user.username}</span></div>
+      <button class="close-3hZ5Ni bf-information" onClick={(e) => {
+        e.cancelBubble = true;
+        e.stopPropagation();
+        const info = data.FRIEND_DATA.lastMessageID[user.id];
+        openModal(() => React.createElement(InformationModal, {
+          user,
+          channel: !info ? 'nothing' : info.channel,
+          message: !info ? 'nothing' : info.id
+        }));
+      }}></button>
     </a>
   </div>
 );
