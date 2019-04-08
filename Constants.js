e7233e59204440bdfb6a340b047aaf082a8fdbb2
Blake <bsward8@gmail.com>
2019-04-08 19:31:21 +1000

    feat: new constants module

---
+++ Constants.js
@@ -0,0 +1,20 @@
+module.exports = ({
+  Statuses: {
+    online: {
+      friendly: 'online',
+      class: 'online-2S838R'
+    },
+    idle: {
+      friendly: 'idle',
+      class: 'idle-3DEnRT'
+    },
+    dnd: {
+      friendly: 'on do not disturb',
+      class: 'dnd-1_xrcq'
+    },
+    offline: {
+      friendly: 'offline',
+      class: 'offline-3qoTek'
+    }
+  }
+});
