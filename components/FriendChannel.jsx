0734321d70cfa9cae2a642940a142bbff1bdf3d6
Blake <bsward8@gmail.com>
2019-04-18 23:29:01 +1000

    fix(FriendChannel.jsx): better support for group dms

--- components/FriendChannel.jsx
+++ components/FriendChannel.jsx
@@ -19,12 +19,11 @@ module.exports = class BetterFriendChannel extends React.Component {
     super();
     this.target = target;
 
-    // bind this to button click event
+    // Bind this to button click event
     this.informationClick = this.informationClick.bind(this);
     this.userClick = this.userClick.bind(this);
   }
 
-  // no usage of "this", no need to bind
   userClick (e) {
     e.stopPropagation();
     e.preventDefault();
@@ -39,16 +38,17 @@ module.exports = class BetterFriendChannel extends React.Component {
     callNewTarget();
     for (const elm of [ ...document.querySelectorAll('.selected-1HYmZZ') ]) {
       elm.classList.remove('selected-1HYmZZ');
-      target.classList.add('selected-1HYmZZ');
     }
 
+    target.classList.add('selected-1HYmZZ');
+
+    // Need to improve how we detect whether or not we're transitioning to a Discord channel or private channel
     if (!target.firstChild.getAttribute('href').includes('undefined')) {
       transitionTo(target.firstChild.getAttribute('href'));
     } else {
       const user = getCurrentUser();
       openPrivateChannel(user.id, this.target.id);
     }
-    target.classList.add('selected-1HYmZZ');
   }
 
   informationClick (e) {
@@ -65,15 +65,19 @@ module.exports = class BetterFriendChannel extends React.Component {
   render () {
     return (() => {
       // Group DM
-      if (this.target.icon) {
+      if (this.target.type === 3) {
         return ((() => (
-          <div className="channel-2QD9_O pc-channel pc-friendchannel" style={{ height: '42px',
+          <div className={`channel-2QD9_O pc-channel pc-friendchannel ${this.target.selected ? 'selected-1HYmZZ' : ''}`} style={{ height: '42px',
             opacity: 1 }}>
             <a href={`/channels/@me/${this.target.id}`} onClick={this.userClick}>
-              <div className="wrapper-2F3Zv8 pc-wrapper small-5Os1Bb pc-small forceDarkTheme-2cI4Hb pc-forceDarkTheme avatar-28BJzY pc-avatar avatarSmall-3ACRaI">
-                <div className="inner-1W0Bkn pc-inner stop-animation" style={{ backgroundImage: `url("https://cdn.discordapp.com/channel-icons/${this.target.id}/${this.target.icon}")` }}></div>
+              <div className='wrapper-2F3Zv8 pc-wrapper small-5Os1Bb pc-small forceDarkTheme-2cI4Hb pc-forceDarkTheme avatar-28BJzY pc-avatar avatarSmall-3ACRaI'>
+                <div className='inner-1W0Bkn pc-inner stop-animation' style={{ backgroundImage: `url('${this.target.icon
+                  ? `https://cdn.discordapp.com/channel-icons/${this.target.id}/${this.target.icon}`
+                  : '/assets/f046e2247d730629309457e902d5c5b3.svg'}')` }}></div>
               </div>
-              <div className="nameWrapper-10v56U"><span className="name-2WpE7M">{this.target.name}</span></div>
+              <div className='name-2WpE7M'>{this.target.name}
+                <div className='activity-525YDR'>{this.target.recipients.length + 1} Members</div>
+              </div>
               <button className='close-3hZ5Ni'></button>
             </a>
           </div>
@@ -82,7 +86,7 @@ module.exports = class BetterFriendChannel extends React.Component {
 
       // This ain't a user, son! This is just a generic channel with a name and SVG avatar.
       if (!this.target.id) {
-        return (<div className="channel-2QD9_O pc-channel pc-friendchannel" style={{ height: '42px',
+        return (<div className={`channel-2QD9_O pc-channel pc-friendchannel ${this.target.selected ? 'selected-1HYmZZ' : ''}`} style={{ height: '42px',
           opacity: 1 }}>
           <a href={this.target.href} onClick={this.userClick}>
             <svg name={this.target.name} className='linkButtonIcon-Mlm5d6' width={this.target.width || '24'} height={this.target.height || '24'} viewBox={this.target.viewBox || '0 0 24 24'}>
@@ -91,13 +95,13 @@ module.exports = class BetterFriendChannel extends React.Component {
                 <rect width='24' height='24'></rect>
               </g>
             </svg>
-            <div className="name-2WpE7M pc-name">{this.target.name}</div>
+            <div className='name-2WpE7M pc-name'>{this.target.name}</div>
             {(() => {
               if (this.target.name === 'Friends') {
                 const rel = getRelationships();
                 const pending = Object.keys(rel).filter(r => rel[r] === 3);
                 if (pending.length) {
-                  return (<div className="wrapper-232cHJ pc-wrapper">{pending.length}</div>);
+                  return (<div className='wrapper-232cHJ pc-wrapper'>{pending.length}</div>);
                 }
               }
             })()}
@@ -107,15 +111,15 @@ module.exports = class BetterFriendChannel extends React.Component {
 
       return ((() => {
         const status = getStatus(this.target.id);
-        return (<div className="channel-2QD9_O pc-channel pc-friendchannel bf-channel" style={{ height: '42px',
+        return (<div className={`channel-2QD9_O pc-channel pc-friendchannel bf-channel ${this.target.selected ? 'selected-1HYmZZ' : ''}`} style={{ height: '42px',
           opacity: 1 }}>
           <a href={`/channels/@me/${getDMFromUserId(this.target.id)}`} onClick={this.userClick}>
-            <div className="wrapper-2F3Zv8 pc-wrapper small-5Os1Bb pc-small forceDarkTheme-2cI4Hb pc-forceDarkTheme avatar-28BJzY pc-avatar avatarSmall-3ACRaI">
-              <div user={this.target.username} status={status} className="inner-1W0Bkn pc-inner stop-animation" style={{ backgroundImage: `url(${this.target.avatarURL})` }}></div>
+            <div className='wrapper-2F3Zv8 pc-wrapper small-5Os1Bb pc-small forceDarkTheme-2cI4Hb pc-forceDarkTheme avatar-28BJzY pc-avatar avatarSmall-3ACRaI'>
+              <div user={this.target.username} status={status} className='inner-1W0Bkn pc-inner stop-animation' style={{ backgroundImage: `url(${this.target.avatarURL})` }}></div>
               <div className={`${Statuses[status].class} status-oxiHuE pc-${status} pc-status small-5Os1Bb pc-small status-2zcSVk pc-status status-1ibiUI pc-status`}></div>
             </div>
-            <div className="nameWrapper-10v56U"><span className="name-2WpE7M">{this.target.username}</span></div>
-            {Plugin.FAV_FRIENDS.includes(this.target.id) && infomodal && <Tooltip className="bf-information-tooltip" text='User Information' position='top'><Info className="bf-information" onClick={this.informationClick} /></Tooltip>}
+            <div className='nameWrapper-10v56U'><span className='name-2WpE7M'>{this.target.username}</span></div>
+            {Plugin.FAV_FRIENDS.includes(this.target.id) && infomodal && <Tooltip className='bf-information-tooltip' text='User Information' position='top'><Info className='bf-information' onClick={this.informationClick} /></Tooltip>}
             {!Plugin.FAV_FRIENDS.includes(this.target.id) && <button className='close-3hZ5Ni'></button>}
           </a>
         </div>);
