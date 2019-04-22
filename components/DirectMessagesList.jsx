104a8367dacdbfdb06cd4a1eb330ae00222b5a6b
Blake <bsward8@gmail.com>
2019-04-23 06:29:28 +1000

    fix(DirectMessagesList.jsx): reee

--- components/DirectMessagesList.jsx
+++ components/DirectMessagesList.jsx
@@ -12,7 +12,6 @@ module.exports = class BetterFriendsDirectMessagesList extends React.Component {
     this.FAV_FRIENDS = thisObj.FAV_FRIENDS;
   }
 
-<<<<<<< HEAD
   async componentDidMount () {
     this.setState({
       getUser: (await getModule([ 'getUser' ])).getUser,
@@ -56,14 +55,6 @@ module.exports = class BetterFriendsDirectMessagesList extends React.Component {
         avatar: { entireElement: <g fill="none" fill-rule="evenodd"><path fill="currentColor" fill-rule="nonzero" d="M0.5,0 L0.5,1.5 C0.5,5.65 2.71,9.28 6,11.3 L6,16 L21,16 L21,14 C21,11.34 15.67,10 13,10 C13,10 12.83,10 12.75,10 C8,10 4,6 4,1.5 L4,0 L0.5,0 Z M13,0 C10.790861,0 9,1.790861 9,4 C9,6.209139 10.790861,8 13,8 C15.209139,8 17,6.209139 17,4 C17,1.790861 15.209139,0 13,0 Z" transform="translate(2 4)"></path><path d="M0,0 L24,0 L24,24 L0,24 L0,0 Z M0,0 L24,0 L24,24 L0,24 L0,0 Z M0,0 L24,0 L24,24 L0,24 L0,0 Z"></path></g> }
       }
     ];
-=======
-  render () {
-    return (
-      <div className='privateChannels-1nO12o'>
-        <div className='searchBar-6Kv8R2'>
-          <SearchBar query='' style={{ width: '100%' }} />
-        </div>
->>>>>>> 0c81db53d4094cb67b1d1132117d61aa5646ecca
 
     const determineIfSelected = id => [ '/activity', '/library', '/store', '/channels/@me' ].includes(id) ? document.location.href === `https://${API_HOST}${id}` : document.location.href.includes(id);
     const channels = [
