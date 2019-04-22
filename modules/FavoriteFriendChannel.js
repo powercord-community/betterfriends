984aedbf0d93e9fd46d085efc5a8df8a51cf9330
Blake <bsward8@gmail.com>
2019-04-23 06:27:32 +1000

    refact(FavoriteFriendChannel.js): major improvements and typing implementation

--- modules/FavoriteFriendChannel.js
+++ modules/FavoriteFriendChannel.js
@@ -1,40 +1,20 @@
 const { inject } = require('powercord/injector');
-const { React } = require('powercord/webpack');
-const { waitFor, getOwnerInstance, sleep } = require('powercord/util');
-const { DirectMessagesList } = require('./../components');
+const { React, getModuleByDisplayName, Flux, getModule } = require('powercord/webpack');
+let { DirectMessagesList } = require('./../components');
 
 /*
  * [ Friend DM Channel ]
  * Creates and populates the "Favorited Friends" section on the private channel/DMs screen
  */
 module.exports = async function () {
-  if (!document.querySelector('.privateChannels-1nO12o')) {
-    await waitFor('.privateChannels-1nO12o');
-  }
+  const PrivateChannelsList = await getModuleByDisplayName('PrivateChannelsList');
+  const typingStore = await getModule([ 'getTypingUsers' ]);
+  const { getCurrentUser } = await getModule([ 'getCurrentUser' ]);
 
-  const updateDirectMessagesInstance = () =>
-    (this.directMessagesInstance = getOwnerInstance(document.querySelector('.privateChannels-1nO12o')));
-  const directMessagesInstance = Object.getPrototypeOf(updateDirectMessagesInstance());
-  updateDirectMessagesInstance();
+  DirectMessagesList = Flux.connectStores(
+    [ typingStore ],
+    ({ id }) => ({ typingCount: Object.keys(typingStore.getTypingUsers(id)).filter(id => id !== getCurrentUser().id).length })
+  )(DirectMessagesList);
 
-  inject('bf-direct-messages-mount', directMessagesInstance, 'componentDidMount', async (args, res) => {
-    await sleep(1);
-    if (document.querySelector('div.channel-2QD9_O')) {
-      updateDirectMessagesInstance();
-      this.directMessagesInstance.render();
-      this.directMessagesInstance.forceUpdate();
-    }
-    return res;
-  });
-
-  inject('bf-direct-messages', directMessagesInstance, 'render', (args, res) => {
-    if (document.querySelector('div.channel-2QD9_O')) {
-      updateDirectMessagesInstance();
-      this.directMessagesInstance.forceUpdate();
-      this.log('Monkeypatching Direct Messages list component');
-      // Implement typing code here
-      return React.createElement(DirectMessagesList, { thisObj: this });
-    }
-    return res;
-  });
+  inject('bf-direct-messages', PrivateChannelsList.prototype, 'render', () => React.createElement(DirectMessagesList, { thisObj: this }));
 };
