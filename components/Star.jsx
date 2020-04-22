2a9b817c61ee9b26cb69057c2c17397e7d6d43dc
Nevvulo <bsward8@gmail.com>
2020-04-23 02:27:48 +1000

    fix: slight changes to star component

--- components/Star.jsx
+++ components/Star.jsx
@@ -2,7 +2,7 @@ const { React } = require('powercord/webpack');
 const { Tooltip } = require('powercord/components');
 
 module.exports = ({ className }) => (
-  <Tooltip className={`bf-star-tooltip ${className}`} text='Favorited Friend' position='top'>
-    <div className='bf-star'></div>
+  <Tooltip className={`bf-star-tooltip`} text='Favorited Friend' position='top'>
+    <div className={`${className} bf-star`}></div>
   </Tooltip>
 );
