0dd898de042cc612d57d64759e4bb6c182e28605
Nevvulo <bsward8@gmail.com>
2020-04-27 03:50:23 +1000

    fix: styling issues with star badge

--- components/StatusHandler.jsx
+++ components/StatusHandler.jsx
@@ -6,7 +6,7 @@ module.exports = ({ status, user, Avatar }) => (
     <div class='bf-status-userinline'>
       <Avatar src={user.avatarURL} status={status} size='SIZE_32' statusTooltip={true} />
       <div style={{ marginLeft: '8px',
-        marginTop: '8px' }} class="bf-status-popup-text">{user.username} is now {Statuses[status].friendly}.</div>
+        marginTop: '8px' }} class="bf-status-popup-text">{user.username} is now {Statuses[status].friendly}</div>
     </div>
   </div>
 );
